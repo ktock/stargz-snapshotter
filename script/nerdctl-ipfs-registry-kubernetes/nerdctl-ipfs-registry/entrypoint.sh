a7ea4b199297e068a501e60fbe820a65bec54e0b
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2021-12-15 10:08:37 +0900

    Node-to-Node image sharing on Kubernetes using nerdctl
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

---
+++ script/nerdctl-ipfs-registry-kubernetes/nerdctl-ipfs-registry/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#   Copyright The containerd Authors.
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+RETRYNUM=100
+RETRYINTERVAL=3
+TIMEOUTSEC=180
+function retry {
+    local SUCCESS=false
+    for i in $(seq ${RETRYNUM}) ; do
+        if eval "timeout ${TIMEOUTSEC} ${@}" ; then
+            SUCCESS=true
+            break
+        fi
+        echo "Fail(${i}). Retrying..."
+        sleep ${RETRYINTERVAL}
+    done
+    if [ "${SUCCESS}" == "true" ] ; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+retry /nerdctl ipfs registry serve
